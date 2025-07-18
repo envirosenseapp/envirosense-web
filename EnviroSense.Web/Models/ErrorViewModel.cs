5f69b2c1ce97dc5531f2f2198492b6ab1f1fb8cc
maxiansandu <maxian.sandu@gmail.com>
2025-07-18 23:30:07 +0300

    created a net SDK project

---
+++ EnviroSense.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EnviroSense.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
