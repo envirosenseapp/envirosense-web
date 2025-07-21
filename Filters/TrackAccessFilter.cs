31ed11b25b477359b472c787721808b070de7c3e
maxiansandu <maxian.sandu@gmail.com>
2025-07-21 15:58:04 +0300

    solving problem with code duplication

--- Filters/TrackAccessFilter.cs
+++ Filters/TrackAccessFilter.cs
@@ -1,36 +1,28 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using EnviroSense.Web.Repositories;
 using EnviroSense.Web.Entities;
+using EnviroSense.Web.Services;
 
 namespace EnviroSense.Web.Filters;
 
 public class AccessTrackingFilter : IAsyncActionFilter
 {
+    private readonly IAccessService _accessService;
+
     private readonly IAccessRepository _accessRepository;
 
-    private readonly IHttpContextAccessor _httpContextAccesor;
-
-    public AccessTrackingFilter(IAccessRepository accessRepository, IHttpContextAccessor httpContextAccesor)
+    public AccessTrackingFilter(IAccessService accessService, IAccessRepository accessRepository)
     {
+        _accessService = accessService;
         _accessRepository = accessRepository;
-        _httpContextAccesor = httpContextAccesor;
+
     }
 
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        var httpContext = _httpContextAccesor.HttpContext;
-
-        var access = new Access
-        {
-            Id = Guid.NewGuid(),
-            CreatedAt = DateTime.UtcNow,
-            IpAddress = httpContext.Connection.RemoteIpAddress.ToString(),
-            Client = httpContext.Request.Headers["User-Agent"].ToString(),
-            Resource = httpContext.Request.Path
-        };
-
+        var access = await _accessService.Create();
         await _accessRepository.CreateAsync(access);
-     
+
 
         await next();
     }
