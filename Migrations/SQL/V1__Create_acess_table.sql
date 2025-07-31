b643f426fed13e5f0dcea700945c1718b5fc8fba
maxiansandu <maxian.sandu@gmail.com>
2025-07-31 14:49:16 +0300

    Trying to fix SQL build failure

--- Migrations/SQL/V1__Create_acess_table.sql
+++ Migrations/SQL/V1__Create_acess_table.sql
@@ -1,8 +1,8 @@
 CREATE
-EXTENSION IF NOT EXISTS "uuid-ossp";
+    EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE accesses
 (
-    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v1(),
-    created_at timestamp NOT NULL DEFAULT current_timestamp
+    id         uuid      NOT NULL PRIMARY KEY DEFAULT uuid_generate_v1(),
+    created_at timestamp NOT NULL             DEFAULT current_timestamp
 )
