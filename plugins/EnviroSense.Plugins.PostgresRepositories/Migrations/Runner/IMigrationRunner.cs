c510e20e4538e0ddde4be6933518a3070fc1da1c
Nicu MAXIAN <nicu@numeral.io>
2025-08-14 19:42:46 +0300

    Remove headers and adjust editorconfig

--- plugins/EnviroSense.Plugins.PostgresRepositories/Migrations/Runner/IMigrationRunner.cs
+++ plugins/EnviroSense.Plugins.PostgresRepositories/Migrations/Runner/IMigrationRunner.cs
@@ -1,8 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-namespace EnviroSense.Plugins.PostgresRepositories.Migrations.Runner;
+﻿namespace EnviroSense.Plugins.PostgresRepositories.Migrations.Runner;
 
 public interface IMigrationRunner
 {
