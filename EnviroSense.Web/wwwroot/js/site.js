e4b380b8513bf80a403a45ee899a07de13de5c0d
maxiansandu <maxian.sandu@gmail.com>
2025-08-20 15:24:41 +0300

    remove unnecessary comment

--- EnviroSense.Web/wwwroot/js/site.js
+++ EnviroSense.Web/wwwroot/js/site.js
@@ -1,7 +1,4 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-function confirmLogout(event) {
+﻿function confirmLogout(event) {
     event.preventDefault(); 
     if (confirm("Are you sure you want to log out?")) {
         window.location.href = '@Url.Action("LogOut", "Accounts")';
