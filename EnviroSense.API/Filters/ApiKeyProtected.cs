07793669207b871336d2e82bc94d48e4fbb3e024
Nicu MAXIAN <nicu@numeral.io>
2025-08-23 00:07:14 +0300

    Add filters for authenticated/unauthenticated requests

---
+++ EnviroSense.API/Filters/ApiKeyProtected.cs
@@ -0,0 +1,24 @@
+﻿using EnviroSense.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EnviroSense.API.Filters;
+
+public class ApiKeyProtected : IActionFilter
+{
+    private readonly IAccountService _accountService;
+
+    public ApiKeyProtected(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        //TODO
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+    }
+}
---
+++ EnviroSense.API/Filters/NoApiKeyProtected.cs
@@ -0,0 +1,24 @@
+﻿using EnviroSense.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EnviroSense.API.Filters;
+
+public class NoApiKeyProtected : IActionFilter
+{
+    private readonly IAccountService _accountService;
+
+    public NoApiKeyProtected(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    public void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        //TODO
+    }
+
+    public void OnActionExecuted(ActionExecutedContext filterContext)
+    {
+    }
+}
