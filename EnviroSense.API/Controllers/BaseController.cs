3e55c704c8b7f06d84f8098b7943a3d876e449b8
Nicu MAXIAN <nicu@numeral.io>
2025-08-23 00:08:37 +0300

    Add basic DTO and utils for api response

--- EnviroSense.API/Controllers/BaseController.cs
+++ EnviroSense.API/Controllers/BaseController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnviroSense.API.Models;
+using EnviroSense.API.Models.Core;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EnviroSense.API.Controllers;
 
@@ -7,5 +9,10 @@ public abstract class BaseController : Controller
     public IActionResult Success<T>(T result)
     {
         return Json(result);
+    }
+    
+    public IActionResult ValidationError()
+    {
+        return BadRequest(new ValidationError(ModelState));
     }
 }
---
+++ EnviroSense.API/Models/Core/ValidationError.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace EnviroSense.API.Models.Core;
+
+public class ValidationError: BaseError{
+    
+    public ValidationError(ModelStateDictionary modelStateDictionary) : base(
+        "validation_error",
+        "validation failed"
+        )
+    {
+        foreach (var state in modelStateDictionary)
+        {
+            foreach (var error in state.Value.Errors)
+            {
+                
+                Context.Add(new Entry(
+                    state.Key,
+                    error.ErrorMessage
+                ));
+            }
+        }
+    }
+}
