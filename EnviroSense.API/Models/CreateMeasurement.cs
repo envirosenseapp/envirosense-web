96aad789e13a85e63973bad515685ea6d9056c4c
Nicu MAXIAN <nicu@numeral.io>
2025-08-24 16:48:37 +0300

    Update validations

--- EnviroSense.API/Models/CreateMeasurement.cs
+++ EnviroSense.API/Models/CreateMeasurement.cs
@@ -1,9 +1,16 @@
-﻿namespace EnviroSense.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnviroSense.API.Models;
 
 public class CreateMeasurement
 {
     public Guid DeviceId { get; set; }
+    
+    [Range(-80, 50)] 
     public float? Temperature { get; set; }
+    
+    [Range(0, 180)] 
     public float? Humidity { get; set; }
+    
     public DateTime RecordingDate { get; set; }
 }
